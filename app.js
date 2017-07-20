5ef02755e76aa653e1cee796af4b46c6ccba9d28
bootcamp <myezasifiso11@gmail.com>
2017-07-20 12:12:19 +0200

    add counter to the application

--- app.js
+++ app.js
@@ -5,13 +5,9 @@ var hbs = require('express-handlebars');
 var cookieParser = require('cookie-parser');
 var flash = require('express-flash');
 var session = require('express-session');
-
 var Greet = require('./greet');
-
 const Models = require('./models');
-
 const models = Models(process.env.MONGO_URL || 'mongodb://localhost/greetedUsers')
-
 //the routes
 let greet = Greet(models);
 //let other = OtherRoutes(models);
@@ -28,57 +24,13 @@ app.set( 'view engine', 'hbs' );
  var greeted = {};
  var counter = 0;
 
- //app.get('/other', other.routes2);
-
  app.get('/greetings', greet.showGreeting);
  app.post('/greetings', greet.addGreeting );
 
-
-
-
-
- //   var name = req.body.name;
- //
- //   console.log(">>>>>>>>>>" + greet.greetUser(name));
- // });
-
- // app.post('/greetings', function(req,res){
- //   var user = req.body.name;
- //   var language = req.body.language;
- //
- //   if(greeted[user] === undefined){
- //      greeted[user] = 0;
- //      counter ++
- //    }
- //    greeted[user] = greeted[user] + 1;
- //    req.flash('info', language + user);
- //    req.flash('count', counter);
- //    res.redirect("greetings");
- // });
- //
- //  app.get('/counter/:user', function (req, res) {
- //    var user = req.params.user;
- //    res.send('Hello'+ " " + user +" "+ 'has been greeted ' + " "+ greeted[user] + ' '+ 'times' )
- //  });
- //
- //  app.get('/greeted', function (req, res) {
- //    var greetedNames =[];
- //    var linkNames =[]
- //    for(key in greeted){
- //      greetedNames.push(key);
- //    };
- //    greetedNames.forEach(function(name){
- //    linkNames.push("<a href=/counter/" + name + ">" + name + "</a>")
- //    });
- //    res.send("Greeted names list " +linkNames)
- //  });
-
-
- app.use(function(err, req, res){
+ app.use(function(err, req, res, next){
    res.send("<h3>Something went wrong!</h3> <pre>" + err.stack + "</pre>")
  });
 
-
-  app.listen(3000, function () {
+app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
-  })
+})
--- greet.js
+++ greet.js
@@ -1,9 +1,15 @@
 let models = null;
+var cookieParser = require('cookie-parser');
+var flash = require('express-flash');
+var session = require('express-session');
+
 
 let addGreeting = function(req, res, next){
+  var language = req.body.language
 
     let User = models.User;
-
+    // var counter = models.User.count();
+    // console.log(counter);
     var actualName = req.body.name;
     var results = ""
      User.findOne({name:actualName}, function(err , person){
@@ -24,8 +30,20 @@ let addGreeting = function(req, res, next){
              else{
                console.log("successfully added new user");
                results = data
-               res.redirect('/greetings');
-                 //return data;
+              //  counter ++
+               // call count here... - what should you do in the Callback?
+              User.count(function(err, counter){
+                 if(err){
+                   console.log(err);
+                   return next(err)
+                 }
+                 else{
+                   console.log(counter);
+                   req.flash('count', counter);
+                   req.flash('info', language + actualName);
+                   res.redirect('/greetings');
+                 }
+               });
              }
 
            });
@@ -39,11 +57,11 @@ let addGreeting = function(req, res, next){
                   console.log(error);
                   return next(err)
                }
-
-               console.log('lets show it!');
+              //  req.flash('count', counter);
+               req.flash('info', language + actualName);
                res.redirect('greetings');
              };
-             
+
              person.save(updateDone);
 
              console.log("Here we go!");
